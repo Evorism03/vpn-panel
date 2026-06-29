f9b2033440d4a95e971f3ed5ccc3f90d013e3573
Evorism03 <linux.evg@gmail.com>
2026-06-29 11:23:20 +0300

    3

--- backend/app/main.py
+++ backend/app/main.py
@@ -130,7 +130,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(GZipMiddleware, minimum_size=512)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
